5b060eff576d0836d0ac1c8db4c40cb2f4ba3b2c
Javier Aceves <jaacevesh@gmail.com>
2024-01-09 20:09:33 -0600

    set until for and range 8.4

---
+++ samples/conversion/frominto.rs
@@ -0,0 +1,50 @@
+// From
+// The From trait allows for a type to define how to create itself from another type, hence providing a very simple mechanism for converting between several types. There are numerous implementations of this trait within the standard library for conversion of primitive and common types.
+
+// For example we can easily convert a str into a String
+
+let my_str = "hello";
+let my_string = String::from(my_str);
+
+// We can do similar for defining a conversion for our own type.
+use std::convert::From;
+
+#[derive(Debug)]
+struct Number {
+    value: i32,
+}
+
+impl From<i32> for Number {
+    fn from(item: i32) -> Self {
+        Number { value: item }
+    }
+}
+
+fn main() {
+    let num = Number::from(30);
+    println!("My number is {:?}", num);
+}
+
+// Into
+// The Into trait is simply the reciprocal of the From trait. That is, if you have implemented the From trait for your type, Into will call it when necessary.
+
+// Using the Into trait will typically require specification of the type to convert into as the compiler is unable to determine this most of the time. However this is a small trade-off considering we get the functionality for free.
+use std::convert::Into;
+
+#[derive(Debug)]
+struct Number {
+    value: i32,
+}
+
+impl Into<Number> for i32 {
+    fn into(self) -> Number {
+        Number { value: self }
+    }
+}
+
+fn main() {
+    let int = 5;
+    // Try removing the type annotation
+    let num: Number = int.into();
+    println!("My number is {:?}", num);
+}
